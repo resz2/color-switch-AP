2ad0a8cbe2ab4f5e2c566ca1358d4892efbd0f28
Saatvik07 <saatvik19097@iiitd.ac.in>
2020-11-26 21:20:15 +0530

    High Score screen animations added, minor bug fixes

--- src/sample/HighScoreController.java
+++ src/sample/HighScoreController.java
@@ -1,16 +1,30 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class HighScoreController {
     @FXML
     private ListView<String> highScoreList;
     @FXML
     private AnchorPane highScoreBG;
+    @FXML
+    private Circle backCircle;
+    @FXML
+    private ImageView background,backIcon,highScoreBanner,highScoreHeading,starImage,difficultyImage;
+
 
     @FXML
     public void initialize()    {
@@ -24,10 +38,65 @@ public class HighScoreController {
         highScoreList.getItems().add("    4                         hard");
         highScoreList.getItems().add("    3                         hard");
         highScoreList.getItems().add("    2                         hard");
+        Timeline enterTimeline = new Timeline(new KeyFrame(Duration.millis(5),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent t) {
+                        background.setOpacity(background.getOpacity()+0.0015);
+                        highScoreBanner.setOpacity(highScoreBanner.getOpacity()+0.01);
+                        highScoreBanner.setLayoutY(highScoreBanner.getLayoutY()+1);
+                        highScoreHeading.setOpacity(highScoreHeading.getOpacity()+0.01);
+                        highScoreHeading.setLayoutY(highScoreHeading.getLayoutY()+1);
+                        highScoreList.setOpacity(highScoreList.getOpacity()+0.01);
+                        difficultyImage.setOpacity(difficultyImage.getOpacity()+0.01);
+                        starImage.setOpacity(starImage.getOpacity()+0.01);
+                        backIcon.setOpacity(backIcon.getOpacity()+0.01);
+                        backIcon.setLayoutX(backIcon.getLayoutX()-2);
+                        backCircle.setOpacity(backIcon.getOpacity()+0.01);
+                        backCircle.setLayoutX(backCircle.getLayoutX()-2);
+                        backIcon.setRotate(backIcon.getRotate()+3.6);
+                    }
+                }));
+        enterTimeline.setCycleCount(100);
+        enterTimeline.play();
+
     }
 
     public void goBack() throws Exception {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
-        highScoreBG.getChildren().setAll(pane);
+        Timeline enterTimeline = new Timeline(new KeyFrame(Duration.millis(5),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent t) {
+                        background.setOpacity(background.getOpacity()-0.0015);
+                        highScoreBanner.setOpacity(highScoreBanner.getOpacity()-0.01);
+                        highScoreBanner.setLayoutY(highScoreBanner.getLayoutY()-1);
+                        highScoreHeading.setOpacity(highScoreHeading.getOpacity()-0.01);
+                        highScoreHeading.setLayoutY(highScoreHeading.getLayoutY()-1);
+                        highScoreList.setOpacity(highScoreList.getOpacity()-0.01);
+                        difficultyImage.setOpacity(difficultyImage.getOpacity()-0.01);
+                        starImage.setOpacity(starImage.getOpacity()-0.01);
+                        backIcon.setOpacity(backIcon.getOpacity()-0.01);
+                        backIcon.setLayoutX(backIcon.getLayoutX()+2);
+                        backCircle.setOpacity(backIcon.getOpacity()-0.01);
+                        backCircle.setLayoutX(backCircle.getLayoutX()+2);
+                        backIcon.setRotate(backIcon.getRotate()-3.6);
+
+                    }
+                }));
+        enterTimeline.setCycleCount(100);
+        enterTimeline.play();
+        enterTimeline.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                AnchorPane pane = null;
+                try {
+                    pane = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                highScoreBG.getChildren().setAll(pane);
+            }
+        });
+
     }
 }
